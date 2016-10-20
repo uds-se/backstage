772b4a036ee70bf1b970358052feb16f7af9a72f
Vitalii Avdiienko <avdiienko@st.cs.uni-saarland.de>
2016-10-20 15:09:42 +0200

    Update runApp.sh

--- runApp.sh
+++ runApp.sh
@@ -10,7 +10,7 @@ $JDK/java -jar apktool_2.1.1.jar -s -f d $file
 cd $CURRENT_DIR
 
 echo "Running analysis for $fileName"
-$JDK/java -Xmx40g -Xss5m -cp target/Backstage-5.1-SNAPSHOT-jar-with-dependencies.jar st.cs.uni.saarland.de.testApps.TestApp -apk ${file} -androidJar $ANDROID_JAR -apkToolOutput output/$appName -rAnalysis -uiTimeoutValue 30 -uiTimeoutUnit SECONDS -rTimeoutValue 30 -rTimeoutUnit SECONDS -rMaxDepthMethodLevel 15 -numThreads 24 -rLimitByPackageName
+$JDK/java -Xmx40g -Xss5m -cp target/Backstage-5.1-SNAPSHOT-jar-with-dependencies.jar st.cs.uni.saarland.de.testApps.TestApp -apk ${file} -androidJar $ANDROID_JAR -apkToolOutput output/$appName -rAnalysis -uiTimeoutValue 30 -uiTimeoutUnit SECONDS -rTimeoutValue 30 -rTimeoutUnit SECONDS -maxDepthMethodLevel 15 -numThreads 24 -rLimitByPackageName
 echo "Analysis for $fileName finished"
 
 cd $CURRENT_DIR/output
